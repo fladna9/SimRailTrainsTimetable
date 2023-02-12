2e54fec6f92e572aeb0472434ed6851f99816c57
Maxence MOHR (fladna9) <18591264+fladna9@users.noreply.github.com>
2023-02-13 00:25:28 +0100

    Refactored and fixed, started the template, added a sample not to trigger a call while working on template.

--- app.py
+++ app.py
@@ -1,24 +1,48 @@
 from flask import Flask, url_for, render_template
 import requests
+import json
+
+STATIC=True
 
 app = Flask(__name__)
 
-timetable_api="https://sr-dispatch-api-staging-uld3cp3nxa-od.a.run.app/train/"
+if __name__ == '__main__':
+    app.run()
 
-url_for('static', filename='css/bootstrap.min.css')
-url_for('static', filename='js/bootstrap.min.js')
+timetable_api="https://sr-dispatch-api-staging-uld3cp3nxa-od.a.run.app/train/"
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Simrail Trains Timetable Frontend'
 
 @app.route('/train/')
 @app.route('/train/<number>')
 def train(number=None):
+    #Setting a default train if none selected
     if not number:
         number = "24181"
-    timetable_response = requests.get(timetable_api + number)
-    stops = timetable_response.json()
-    return render_template('train.html', number=number, stops=stops)
-if __name__ == '__main__':
-    app.run()
+
+    #Getting static or API
+    if STATIC:
+        with open('examples/24181.json', 'r') as samplefile:
+            stops = json.load(samplefile)
+        dataisfrom = "API"
+    else:
+        timetable_response = requests.get(timetable_api + number)
+        stops = timetable_response.json()
+        dataisfrom = "Static"
+
+    #Making static files available
+    bs_css = url_for('static', filename='css/bootstrap.min.css')
+    bs_js = url_for('static', filename='js/bootstrap.min.js')
+
+    #Building template
+    return render_template('train.html', number=number, stops=stops, bootstrapcss=bs_css, bootstrapjs=bs_js, datafrom=dataisfrom)
+
+
+def create_app():
+    app = Flask(__name__)
+
+    with app.app_context():
+        print('hello')
+    return app
