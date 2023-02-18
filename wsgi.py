b54dbfb3b1e6964430bcb6f4d33e38c90cd5abfd
Max <18591264+fladna9@users.noreply.github.com>
2023-02-18 14:59:42 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,25 @@
-from app import app
-if __name__ == "__main__":
-   app.run()
+from flask import Flask, url_for, render_template
+import app
+import traceback
+import uuid
+
+STATIC=False
+
+app = Flask(__name__)
+
+
+from routes import train, \
+    train_selector, \
+    train_position, \
+    error_handlers
+
+
+def create_app():
+    app = Flask(__name__)
+    with app.app_context():
+        print('hello')
+    return app
+
+
+if __name__ == '__main__':
+    app.run()
